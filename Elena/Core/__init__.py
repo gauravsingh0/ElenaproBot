d843eda1da084bbacd59ac7a8989e4b074f64d14
Gaurav singh <84321170+gauravsingh0@users.noreply.github.com>
2022-01-10 00:44:38 +0530

    Update __init__.py

--- Elena/Core/__init__.py
+++ Elena/Core/__init__.py
@@ -13,5 +13,5 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from .Elena import Elena
+from .elena import Elena
 from .logging import *
